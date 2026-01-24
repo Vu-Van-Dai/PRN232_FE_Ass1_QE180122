89c7bb71a484eaac48599414c26e09f38ebd08c0
VŨ VĂN ĐẠI <Daivvqe180122@fpt.edu.vn>
2026-01-24 15:50:39 +0700

    final of final of final

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,4 +1,4 @@
-const DEFAULT_API_BASE_URL = "http://localhost:5063";
+const DEFAULT_API_BASE_URL = "https://prn232-ass1-qe180122.onrender.com";
 
 export const API_BASE_URL: string =
   (import.meta.env.VITE_API_BASE_URL as string | undefined) ?? DEFAULT_API_BASE_URL;
